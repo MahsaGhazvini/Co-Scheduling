c8102048f32144d46385d67b4869cbb26166d37a
mahsa <mahsa.ghazvini@gmail.com>
2018-12-20 18:41:36 +0330

    some refactore on voteList

--- Backend/routes/vote/voteList.js
+++ Backend/routes/vote/voteList.js
@@ -5,36 +5,22 @@ const VotingRight = require('../../models/VotingRight');
 const PollForm = require('../../models/PollForm');
 
 router.get('/', function(req, res, next) {
-    let itemsProcessed = 0;
-    const resJson = {
-        data: []
-    };
-
     VotingRight.findAll({
         where:{
             userId: req.query.email
+        },
+        include:{
+            model: PollForm,
+            required: false,
         }
-    }).then(async function(votingRights){
-        if(votingRights.length === 0)
-            res.status(200).json(resJson);
-        else {
-            await votingRights.forEach(async votingRight => {
-                let pollFormId = votingRight.pollFormId;
-                await PollForm.findOne({
-                    where: {
-                        id: pollFormId
-                    }
-                }).then(pollForm => {
-                    resJson.data.push({
-                        id: pollFormId,
-                        title: pollForm.dataValues.title,
-                        description: pollForm.dataValues.description
-                    });
-                });
-                if(++itemsProcessed === votingRights.length)
-                    res.status(200).json(resJson);
-            });
-        };
+    }).then(function (result) {
+        res.status(200).json(result.map(r=>{
+            return {
+                "id": r.pollForm.dataValues.id,
+                "title": r.pollForm.dataValues.title,
+                "des": r.pollForm.dataValues.description
+            };
+        }));
     });
 });
 
