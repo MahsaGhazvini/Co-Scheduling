2fb44d196a424ce27f7e23d0d1f8eb2e83ee1633
mahsa <mahsa.ghazvini@gmail.com>
2018-12-23 16:34:38 +0330

    start react

---
+++ react_client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom'
+
+import HomePage from './HomePage/HomePage'
+
+const App = () =>
+    <Router>
+        <div>
+            <Route exact path="/" component={HomePage}/>
+        </div>
+    </Router>
+
+export default App
---
+++ react_client/src/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                salam
+            </div>
+        );
+    }
+}
+
+export default HomePage;
