5bb4821fbf4cd61c0c6e0213c3d49c8f4c897840
mahsa <mahsa.ghazvini@gmail.com>
2018-12-24 08:58:03 +0330

    see result of voting for creator of polls.

--- Backend/routes/managePolls/index.js
+++ Backend/routes/managePolls/index.js
@@ -1,8 +1,15 @@
 const Sequelize = require('sequelize');
 const express = require('express');
 const manageList = require('./manageList');
+const managePage = require('./managePage');
+
 const router = express.Router();
 
 router.use('/', manageList);
+router.use('/:id/', function(req, res, next){
+    req.pollId = req.params.id;
+    return next();
+});
+router.use('/:id/', managePage);
 
 module.exports = router;
