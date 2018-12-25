e1c2b4e80cb4597f5d8b7769ffa025da1b8eaf67
saharsamr <sahar.rajabi76@gmail.com>
2018-12-25 08:02:16 +0330

    add test for checking the owner of poll.

--- Backend/test/routes/managePolls/closePoll.spec.js
+++ Backend/test/routes/managePolls/closePoll.spec.js
@@ -30,11 +30,19 @@ describe('POST /managePolls/:id/:pollOptionId', function () {
         { 'logging': false }
     );
 
-    it('should create a new poll', function(done) {
+    it('should close a poll', function(done) {
         request
             .post('/managePolls/2/2?email=saharsamr@gmail.com')
             .set('Accept', 'application/json')
             .expect('Content-Type', /html/)
             .expect(200, done);
+    });
+
+    it('should rise error when the poll is not yours', function(done) {
+        request
+            .post('/managePolls/3/6?email=saharsamr@gmail.com')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /text/)
+            .expect(403, done);
     });
 });
