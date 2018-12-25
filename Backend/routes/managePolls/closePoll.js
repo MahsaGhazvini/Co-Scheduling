adfcb6d8fc1253cd7069ef39dbe17027f1cdb15a
mahsa <mahsa.ghazvini@gmail.com>
2018-12-25 11:47:49 +0330

    closePoll serverSide changed

--- Backend/routes/managePolls/closePoll.js
+++ Backend/routes/managePolls/closePoll.js
@@ -8,30 +8,30 @@ const VotingRight = require('../../models/VotingRight');
 const mail = require('../../services/sendingEmail');
 
 router.post('/', function(req, res, next) {
-    const email = req.query.email;
+    const email = req.body.email;
     const formId = req.pollId;
     const optionId = req.optionId;
 
     PollForm.findById(formId).then(form => {
         if(form.creator !== email)
-            return res.send(403);
+            return res.send(403).json({"message":"Just admin can close the poll"});
         else if(form.active === false)
-            return res.status(400).send('Poll form ended before!');
+            return res.status(400).json({"message":'Poll form ended before!'});
         else{
             PollOption.findById(optionId).then(option =>{
                 if(option.pollFormId != formId)
-                    return res.status(400).send('Unmatched input parameter');
+                    return res.status(400).json({"message":'Unmatched input parameter'});
                 else{
                     option.isFinalized = true;
                     option.save();
                     form.active= false;
                     form.save();
                     mail({user: "user", pass: "pass"},findMembers(formId), "close", );
-                    return res.status(200).send('Successful');
+                    return res.status(200).json({"message":'Successful'});
                 }
             });
         }
-    }).catch(error => {return res.status(400).send('Poll form not exist')})
+    }).catch(error => {return res.status(400).json({"message":'Poll form not exist'})})
 
     const findMembers = async function (pollId) {
         let votingRights = await VotingRight.findAll({
