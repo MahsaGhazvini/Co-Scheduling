7b6592a5c18f84655f69a9fcf5f53c93dd99a085
mahsa <mahsa.ghazvini@gmail.com>
2019-01-02 11:19:44 +0330

    complete frontend of edit page

--- react_client/src/AddPoll/AddPoll.js
+++ react_client/src/AddPoll/AddPoll.js
@@ -1,74 +1,19 @@
 import React, { Component } from 'react';
-import DefaultNavbar from '../Common/DefaultNavbar';
-import TitleComponent from '../Common/TitleComponent';
 import * as Network from "../Common/RequestMaker";
-import Footer from "../Common/Footer";
-
-//const request = require('request');
+import InformationBox from "../Common/InformationBox";
 
 class HomePage extends Component {
     constructor(props){
         super(props);
 
-        this.state = {
-            title: (this.props.title)? this.props.title : '',
-            description: (this.props.description)? this.props.description : '',
-            members_email: (this.props.members_email)? this.props.members_email : [],
-            options_title: (this.props.options_title)? this.props.options_title : [],
-            server: (this.props.server === "editPoll")? "editPoll": "createPoll"
-        };
-        this.handleChange = this.handleChange.bind(this);
+        this.getValue = this.getValue.bind(this);
         this.submitForm = this.submitForm.bind(this);
-        this.newItem = this.newItem.bind(this);
-        this.addMail = this.addMail.bind(this);
-        this.addOption = this.addOption.bind(this);
         this.getVisibleElements = this.getVisibleElements.bind(this);
-        this.getChangeElement = this.getChangeElement.bind(this);
     }
 
-    componentDidMount(){
-        if(this.props.options){
-            var i;
-            for(i=0;i<this.props.options.length;i++){
-                var option = this.props.options[i];
-                var event;
-                this.newItem(event,"addOption",option.data.description, "old");
-            }
-        }
-    }
-
-    newItem(event,inputName, inputValue, newOrOld){
-        var li = document.createElement("li");
-        li.className = inputName+" "+newOrOld;
-        var t = document.createTextNode(inputValue);
-        li.appendChild(t);
-        if (inputValue === '') {
-            alert("You must write something!");
-        } else {
-            document.getElementById("myUL-"+inputName).appendChild(li);
-        }
-        document.getElementById(inputName).value = "";
-
-
-        var span = document.createElement("SPAN");
-        var txt = document.createTextNode("\u00D7");
-        span.className = "close";
-        span.appendChild(txt);
-        li.appendChild(span);
-        span.onclick = function() {
-            var div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
-
-    addMail(event){
-        var inputValue = document.getElementById("addMail").value;
-        this.newItem(event,"addMail", inputValue, "new");
-    }
-
-    addOption(event){
-        var inputValue = document.getElementById("addOption").value;
-        this.newItem(event,"addOption",inputValue, "new");
+    getValue(idName){
+        var node = document.getElementById(idName);
+        return node.value;
     }
 
     getVisibleElements(inputName){
@@ -83,96 +28,27 @@ class HomePage extends Component {
         return arr;
     }
 
-    getChangeElement(inputName, oldOrNew, display){
-        var arr = [];
-        var myNodelist = document.getElementsByClassName(inputName+" "+oldOrNew);
-        var i;
-        for (i = 0; i < myNodelist.length; i++) {
-            var div = myNodelist[i];
-            if(div.style.display === display || (display === 'block' && div.style.display !== 'none'))
-                arr.push(div.childNodes[0].nodeValue);
-        }
-        return arr;
-    }
-
-    handleChange(event) {
-        this.setState({[event.target.name]: event.target.value});
-    }
-
     submitForm(event){
-        if(this.state.server === "createPoll"){
-            let data = new URLSearchParams();
-            this.state.members_email = this.getVisibleElements("addMail").map(mail=> {return {"email": mail}});
-            this.state.options_title = this.getVisibleElements("addOption").map(option => {return {"title": option}});
-            data.append("creator", JSON.stringify({email: localStorage.getItem("email")}));
-            data.append("title", this.state.title);
-            data.append("description", this.state.description);
-            data.append("members", JSON.stringify(this.state.members_email));
-            data.append("options", JSON.stringify(this.state.options_title));
-            Network.PostRequest('http://localhost:3000/createPoll', data).then((res)=>{
-                console.log(res);
-                this.props.history.push({pathname: '/listPage'});
-            });
-        }
-        else{
-            const deleted = this.getChangeElement("addOption","old","none");
-            const added = this.getChangeElement("addOption","new","block");
-            console.log(this.state.title, this.state.description, "-> ",deleted, added);
-            let data = new URLSearchParams();
-            data.append("title", this.state.title);
-            data.append("description", this.state.description);
-            data.append("deletedOptions", JSON.stringify(deleted));
-            data.append("addedOptions", JSON.stringify(added));
-            data.append("editorMail", localStorage.getItem("email"));
-            Network.PostRequest('http://localhost:3000/editPoll', data).then((res)=>{
-                console.log(res);
-                this.props.history.push({pathname: '/listPage'});
-            });
-        }
+        let data = new URLSearchParams();
+        const members_email = this.getVisibleElements("addMail").map(mail=> {return {"email": mail}});
+        const options_title = this.getVisibleElements("addOption").map(option => {return {"title": option}});
+        data.append("creator", JSON.stringify({email: localStorage.getItem("email")}));
+        data.append("title", this.getValue("title"));
+        data.append("description", this.getValue("description"));
+        data.append("members", JSON.stringify(members_email));
+        data.append("options", JSON.stringify(options_title));
+        Network.PostRequest('http://localhost:3000/createPoll', data).then((res)=>{
+            console.log(res);
+            this.props.history.push({pathname: '/listPage'});
+        });
     }
 
     render() {
         return (
-            <div className="body-add-poll">
-                <DefaultNavbar/>
-                <TitleComponent title={(this.state.server === "editPoll")? "ویرایش نظرسنجی" :"اضافه کردن نظرسنجی جدید"}/>
-
-                <div className="container description">
-                    <div className="row">
-                    <div className="input-box col-md-6">
-                        <input type="text" id="title" onChange={this.handleChange} value={this.state.title} className="input-tag input-radius" name="title" placeholder="عنوان"/>
-                    </div>
-                    <div className="input-box margin-bottom col-md-6">
-                        <input type="text" id="description" onChange={this.handleChange} value={this.state.description} className="input-tag input-radius" name="description" placeholder="توضیحات"/>
-                    </div>
-                    </div>
-
-
-                <div className=" row margin-bottom arrayShow">
-                    <div className=" input-box col-md-6">
-
-                        <div id="myDIV" className="header">
-                            <input type="text" id="addOption" placeholder="گزینه بعدی را وارد کنید..."/>
-                            <span onClick={this.addOption} className="addBtn">Add</span>
-                        </div>
-
-                        <ul id="myUL-addOption"> </ul>
-                    </div>
-
-                    <div className="input-box col-md-6" style={{"display":(this.props.title)?"none":"block"}}>
-                        <div id="myDIV" className="header">
-                            <input type="text" id="addMail" placeholder="ایمیل بعدی را وارد کنید..."/>
-                            <span onClick={this.addMail} className="addBtn">Add</span>
-                        </div>
-
-                        <ul id="myUL-addMail"> </ul>
-                    </div>
-
-
-                </div>
-                    <div className="">
-                        <input type="submit" value={(this.props.titleComponent)? this.props.titleComponent : "ثبت نظرسنجی"} onClick={this.submitForm} className="submit add-poll-submit"/>
-                    </div>
+            <div>
+                <InformationBox title='' description='' server={"editPoll"}/>
+                <div className="container">
+                    <input type="submit" value={"ثبت نظرسنجی"} onClick={this.submitForm} className="submit add-poll-submit"/>
                 </div>
             </div>
         );
--- react_client/src/EditPoll/EditPoll.js
+++ react_client/src/EditPoll/EditPoll.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import * as Network from '../Common/RequestMaker';
-import DefaultNavbar from '../Common/DefaultNavbar';
-import TitleComponent from '../Common/TitleComponent';
 
-// import ManageOptionList from './ManageOptionList';
 import './../Styles/common.css';
-import AddPoll from '../AddPoll/AddPoll';
-import PollShow from "../ListPage/PollShow";
-import PollList from "../ListPage/PollList";
+import InformationBox from '../Common/InformationBox';
+
 
 class ManagePolls extends Component {
     constructor(){
@@ -20,6 +16,42 @@ class ManagePolls extends Component {
             description: '',
             title: ''
         }
+        this.submitForm = this.submitForm.bind(this);
+        this.getChangeElement = this.getChangeElement.bind(this);
+        this.getValue = this.getValue.bind(this);
+    }
+
+    getChangeElement(inputName, oldOrNew, display){
+        var arr = [];
+        var myNodelist = document.getElementsByClassName(inputName+" "+oldOrNew);
+        var i;
+        for (i = 0; i < myNodelist.length; i++) {
+            var div = myNodelist[i];
+            if(div.style.display === display || (display === 'block' && div.style.display !== 'none'))
+                arr.push(div.childNodes[0].nodeValue);
+        }
+        return arr;
+    }
+
+    getValue(idName){
+        var node = document.getElementById(idName);
+        return node.value;
+    }
+
+    submitForm(event){
+        const deleted = this.getChangeElement("addOption","old","none");
+        const added = this.getChangeElement("addOption","new","block");
+        console.log(this.state.title, this.state.description, "-> ",deleted, added);
+        let data = new URLSearchParams();
+        data.append("title", this.getValue("title"));
+        data.append("description", this.getValue("description"));
+        data.append("deletedOptions", JSON.stringify(deleted));
+        data.append("addedOptions", JSON.stringify(added));
+        data.append("editorMail", localStorage.getItem("email"));
+        Network.PostRequest('http://localhost:3000/editPoll', data).then((res)=>{
+            console.log(res);
+            this.props.history.push({pathname: '/listPage'});
+        });
     }
 
     componentWillMount(){
@@ -48,7 +80,12 @@ class ManagePolls extends Component {
     render() {
         if(this.state.title)
         return (
-            <AddPoll title={this.state.title} description={this.state.description} options={this.state.options} server={"editPoll"}/>
+            <div>
+                <InformationBox title={this.state.title} description={this.state.description} options={this.state.options} server={"editPoll"}/>
+                <div className="container">
+                    <input type="submit" value={"اعمال ویرایش"} onClick={this.submitForm} className="submit add-poll-submit"/>
+                </div>
+            </div>
         );
         else return (<div/>)
     }
